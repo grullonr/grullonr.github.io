37b249a9dffd016bc7480b7f236076d2aeba1acf
grullonr <rafael.grullon@lc.cuny.edu>
2023-02-27 21:40:19 -0500

    modified App.js to say {insert your name here}

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          My name is Rafael Grullon and I am going to master React!
         </p>
         <a
           className="App-link"
